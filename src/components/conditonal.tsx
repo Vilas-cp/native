9ff9ec60a2e6192c897e4cff33ed458a836e6d4a
Vilas-cp <vilaspgowda1000@gmail.com>
2024-09-16 13:32:56 +0530

    ternary

--- src/components/conditonal.tsx
+++ src/components/conditonal.tsx
@@ -1,29 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
 
-const Valid = () => 
- { return <Text>Valid</Text>;}
+const Valid = () => {
+  return <Text>Valid</Text>;
+};
 
-const Invalid = () => 
-  <Text>Not valid</Text>;
+const Invalid = () => <Text>Not valid</Text>;
 
+const Password = ({ isvalid }: any) => {
+  if (isvalid) {
+    return <Valid />;
+  }
+  return <Invalid />;
+};
 
-const Password=({isvalid}:any)=>{
-    if(isvalid){
-        return <Valid/>;
-    }
-    return <Invalid/>;
-}
-
-
+const grocery: string | ArrayLike<any> | null | undefined= ["hello","help","bye"];
 
 const Conditonal = () => {
   return (
     <View>
       <Text>conditonal</Text>
-      <Password isvalid={false}/>
-
-
+      <Password isvalid={false} />
+{grocery.length>0? <FlatList
+        data={grocery}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        keyExtractor={(item) => item}
+      />:<Text>No item found</Text>}
+      {grocery.length>0&&(<Text>{grocery.length} items in your cart</Text>)}
+     
     </View>
   );
 };
